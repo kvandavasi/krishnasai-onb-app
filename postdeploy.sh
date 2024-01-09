2f81501b82dd286b3863aa1af22c70e00006fefe
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2024-01-09 15:03:45 +0530

    Create postdeploy.sh

---
+++ postdeploy.sh
@@ -0,0 +1,8 @@
+# clear expedited cdn cache (this url is for staging)
+# no longer using CDN so commenting below out on 10-31-2023
+# curl https://app.expeditedsecurity.com/clear/94xCpq4DI17Y60L4TOZLlMXAyBywVgzM
+
+#
+echo $Hi
+echo $HEROKU_APP_NAME
+echo "the postdeploy.sh script has been run"% 
